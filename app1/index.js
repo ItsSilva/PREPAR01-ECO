bde52ff990c868ebebbad9f0eb6292080e8eee7e
Juan Silva <juanse909@hotmail.com>
2025-03-14 09:06:47 -0500

    feat: value of for cache and send by POST method

--- app1/index.js
+++ app1/index.js
@@ -9,6 +9,29 @@ function getUsers() {
     .catch((error) => console.error("Error:", error));
 }
 
+//Register User
+const registerUser = async (event) => {
+  event.preventDefault();
+
+  const name = document.getElementById("user-name").value;
+  console.log(name);
+
+  const response = await fetch("http://localhost:5050/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name }),
+  });
+  const data = await response.json();
+  if (response.ok) {
+    console.log("User created", data);
+  } else {
+    console.error("Error", data);
+  }
+};
+document.getElementById("start-btn").addEventListener("click", registerUser);
+
 const sendCoordenates = () => {
   socket.emit("coordenadas", { x: 123, y: 432 });
 };
