54092c94102c36d9492675d9226a810bcd49f6bf
Juan Silva <juanse909@hotmail.com>
2025-03-14 09:06:16 -0500

    feat: post method register users added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const { Server } = require("socket.io");
 const { createServer } = require("http");
+const { error } = require("console");
 
 const app = express();
 
@@ -20,8 +21,32 @@ app.use("/app1", express.static(path.join(__dirname, "app1")));
 
 let users = [];
 
+//Get route to get all users
 app.get("/users", (req, res) => {
   res.send(users);
+});
+
+//Post route to create a new user
+app.post("/users", (req, res) => {
+  const { name } = req.body;
+  console.log(name);
+
+  if (!name) {
+    return res.status(400).json({ error: "Name is required" });
+  }
+
+  const id = users.length + 1;
+
+  const roleList = ["Marco", "Polo", "Special-Polo"];
+
+  const getRandomRole = () => {
+    const randomRoles = Math.floor(Math.random() * roleList.length);
+    return roleList[randomRoles];
+  };
+
+  const user = { id, name, role: getRandomRole() };
+  users.push(user);
+  res.status(201).json(user);
 });
 
 io.on("connection", (socket) => {
