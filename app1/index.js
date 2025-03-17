02e2f105727d8ff70c9502db68337a64dcd3c660
Juan Silva <juanse909@hotmail.com>
2025-03-17 09:55:53 -0500

    feat: socket added not working

--- app1/index.js
+++ app1/index.js
@@ -1,36 +1,39 @@
 const socket = io("http://localhost:5050", { path: "/rea-time" });
 
-document.getElementById("get-btn").addEventListener("click", getUsers);
-
-function getUsers() {
-  fetch("http://localhost:5050/users")
-    .then((response) => response.json())
-    .then((data) => console.log("get response", data))
-    .catch((error) => console.error("Error:", error));
-}
-
-// Rol screen
-const rolScreen = (data) => {
-  const rolContainer = document.getElementById("container-rol");
-  rolContainer.style.display = "block";
-
-  rolContainer.innerHTML = "";
-  rolContainer.innerHTML = `
-  <h1>${data.name}</h1>
-  <p>Your rol is:</p>
-  <h2>${data.role}</h2>
-  <p>Waiting for more players to join...</p>
-  `;
+// Load screen
+const loadScreen = () => {
+  const loadContainer = document.getElementById("load-container");
+  loadContainer.style.display = "block";
+  loadContainer.innerHTML = `
+    <p>Waiting for other players to connect</p>
+    `;
 };
 
-// Change to rol screen
-const changeToTheRolScreen = (data) => {
+// Change to game screen
+const changeToTheGameScreen = (data) => {
+  const loadContainer = document.getElementById("load-container");
+  loadContainer.style.display = "none";
+
+  const gameContainer = document.getElementById("container-rol");
+  gameContainer.style.display = "block";
+
+  const usersList = document.getElementById("users-list");
+  usersList.innerHTML = "";
+  data.forEach((user) => {
+    const userElement = document.createElement("li");
+    userElement.textContent = `${user.name} - ${user.role}`;
+    usersList.appendChild(userElement);
+  });
+};
+
+// Change to load screen
+const changeToTheLoadScreen = (data) => {
   const nameInput = document.getElementById("user-name");
   const registerContainer = document.getElementById("container-start");
 
   if (nameInput !== null && nameInput.value) {
     registerContainer.style.display = "none";
-    rolScreen(data);
+    loadScreen(data);
   } else {
     alert("Name is required");
   }
@@ -54,15 +57,29 @@ const registerUser = async (event) => {
   const data = await response.json();
   if (response.ok) {
     console.log("User created", data);
-    changeToTheRolScreen(data);
+    changeToTheLoadScreen(data);
   } else {
     console.error("Error", data);
   }
+
+  // Start the game with post and socket
+  if (data.length >= 3) {
+    const response = await fetch("http://localhost:5050/start-game", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({}),
+    });
+
+    const data = await response.json();
+    if (response.ok) {
+      socket.emit("start-game", changeToTheGameScreen(data));
+      console.log("Game started", data);
+    } else {
+      console.error("Error", data);
+    }
+  }
 };
+
 document.getElementById("start-btn").addEventListener("click", registerUser);
-
-const sendCoordenates = () => {
-  socket.emit("coordenadas", { x: 123, y: 432 });
-};
-
-document.getElementById("event-btn").addEventListener("click", sendCoordenates);
