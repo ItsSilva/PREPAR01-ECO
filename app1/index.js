0e1291edc4c9bd595c81897798ef63514f7b7940
Juan Silva <juanse909@hotmail.com>
2025-03-17 08:43:33 -0500

    feat: Change content order for initialization

--- app1/index.js
+++ app1/index.js
@@ -9,7 +9,34 @@ function getUsers() {
     .catch((error) => console.error("Error:", error));
 }
 
-//Register User
+// Rol screen
+const rolScreen = (data) => {
+  const rolContainer = document.getElementById("container-rol");
+  rolContainer.style.display = "block";
+
+  rolContainer.innerHTML = "";
+  rolContainer.innerHTML = `
+  <h1>${data.name}</h1>
+  <p>Your rol is:</p>
+  <h2>${data.role}</h2>
+  <p>Waiting for more players to join...</p>
+  `;
+};
+
+// Change to rol screen
+const changeToTheRolScreen = (data) => {
+  const nameInput = document.getElementById("user-name");
+  const registerContainer = document.getElementById("container-start");
+
+  if (nameInput !== null && nameInput.value) {
+    registerContainer.style.display = "none";
+    rolScreen(data);
+  } else {
+    alert("Name is required");
+  }
+};
+
+// Register User
 const registerUser = async (event) => {
   event.preventDefault();
 
@@ -23,33 +50,15 @@ const registerUser = async (event) => {
     },
     body: JSON.stringify({ name }),
   });
+
   const data = await response.json();
   if (response.ok) {
     console.log("User created", data);
+    changeToTheRolScreen(data);
   } else {
     console.error("Error", data);
   }
-  //Change to rol screen
-  const changeToTheRolScreen = () => {
-    const nameInput = document.getElementById("user-name");
-    const registerContainer = document.getElementById("container-start");
-
-    if (nameInput !== null && nameInput.value) {
-      registerContainer.style.display = "none";
-      rolScreen();
-    } else {
-      alert("Name is required");
-    }
-  };
-  changeToTheRolScreen();
 };
-
-//Rol screen
-const rolScreen = () => {
-  const rolContainer = document.getElementById("container-rol");
-  rolContainer.style.display = "block";
-};
-
 document.getElementById("start-btn").addEventListener("click", registerUser);
 
 const sendCoordenates = () => {
